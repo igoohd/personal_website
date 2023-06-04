eecb458d4a249292193675d300667a27a5ed8624
igoohd <igor.duartematos@gmail.com>
2023-06-04 00:37:39 -0300

    feat: Config vue i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   pages: true,
-  modules: ["@nuxtjs/tailwindcss"],
-  css: [
-    "assets/styles/global.css",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  i18n: {
+    defaultLocale: "pt",
+    langDir: "locales/",
+    locales: [
+      {
+        code: "pt",
+        file: "pt.json",
+      },
+      {
+        code: "en",
+        file: "en.json",
+      },
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',  // recommended
+    }  
+  },
+  css: ["assets/styles/global.css"],
 });

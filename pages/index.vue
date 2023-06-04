4978fe8f8682466273329bd83262aad094bfe7ca
igoohd <igor.duartematos@gmail.com>
2023-06-03 21:36:59 -0300

    feat: Create initial  config of project

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-longing">tesste</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'longing': '#FFF4EA'
+      },
+    },
+  },
+};

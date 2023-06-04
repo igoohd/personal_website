8217deae987fd8b6317fac8330b87afac2cb3cd8
igoohd <igor.duartematos@gmail.com>
2023-06-03 21:53:21 -0300

    feat: set theme color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        'longing': '#FFF4EA'
+    colors: {
+      beige: {
+        50: 'F8F4E9',
+        100: '#FFF4EA',
+        200: '#FAF0E6',
+        300: '#E3D0BE'
       },
+      orange: {
+        50: '#F1E4D8',
+        100: '#FFCAA7',
+        200: '#FF9349',
+        300: '#FF6B00'
+      },
+      brown: {
+        100: '#7A624C'
+      },
+      gray: {
+        100: '#7D7D7D'
+      }
     },
   },
 };

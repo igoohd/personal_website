177336dd69db6350a6dbba2c8dd64d32aaf16f22
Igor Duarte <igor.duartematos@gmail.com>
2023-11-03 09:58:25 -0300

    feat: Corrige responsividade e tamanho de fontes

--- components/Section/Contact/Contact.vue
+++ components/Section/Contact/Contact.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="bg-beige-100 min-h-screen bg-right-to-left-waves bg-[length:848px_406px] sm:bg-[length:auto] bg-[right_bottom] bg-no-repeat relative"
+    class="bg-beige-100 min-h-screen bg-right-to-left-waves bg-no-repeat bg-[length:848px_406px] md:bg-[length:100%] bg-[right_bottom] relative"
   >
     <NuxtLayout>
       <div class="select-none flex flex-col gap-2 mt-10 sm:absolute sm:top-1/2 sm:-translate-y-1/2">
-        <h1 class="text-[7vw] sm:text-[3vw]">{{ $t("role") }}</h1>
-        <h2 class="text-[8vw] sm:text-[4vw] font-bold">Igor Duarte</h2>
-        <Button class="w-6/12">Contate-me</Button>
+        <h1 class="text-2xl sm:text-4xl mb-1 sm:mb-4">{{ $t("role") }}</h1>
+        <h2 class="text-4xl sm:text-6xl font-bold mb-px sm:mb-3">Igor Duarte</h2>
+        <Button class="w-32 sm:w-48 text-base sm:text-2xl">Contate-me</Button>
       </div>
       <div class="flex absolute bottom-2.5 mx-auto inset-x-0 w-max">
         <SVGArrowScroll class="cursor-pointer"/>
